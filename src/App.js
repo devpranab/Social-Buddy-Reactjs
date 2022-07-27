b7a6ab373682245c4aa1e1df2c6a97c9056bd655
pranav s <dev.pranabsk@gmail.com>
2022-07-27 10:21:17 +0530

    first commit on social buddy

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+import './App.css';
+import About from "./components/About/About";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Home from "./components/Home/Home";
+import PostDetails from "./components/PostDetails/PostDetails";
+
+function App() {
+  // let history = useParams();
+  // function handleClick() {
+  //   console.log('click');
+  // }
+  return (
+    <div className='App'>
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/post/:id">
+            <PostDetails></PostDetails>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
